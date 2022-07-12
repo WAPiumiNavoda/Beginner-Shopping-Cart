c78f46d0b7a3522656f5fe3b8c29368cd40e4c15
Piumi Navoda <it20617028@my.sliit.lk>
2022-07-12 11:11:26 +0530

    finished

--- src/component/Cart.js
+++ src/component/Cart.js
@@ -34,8 +34,12 @@ function Cart() {
                         <td>{item.price}</td>
                         <td>Quantity: {item.quantity}</td>
                         <td>
-                          <button>-</button>
-                          <button>+</button>
+                          <button className='btn btn-info ms-2' 
+                           onClick={()=> updateItemQuantity(item.id ,item.quantity -1)}>-</button>
+                          <button className='btn btn-info ms-2' 
+                           onClick={()=> updateItemQuantity(item.id ,item.quantity +1)}>+</button>
+                          <button className='btn btn-danger ms-2'
+                           onClick={()=> removeItem(item.id)}>Remove</button>
                         </td>
                     </tr>
                   )
@@ -44,7 +48,16 @@ function Cart() {
                   }
                 </tbody>
             </table>
-            
+        </div>
+        <div className='col-auto ms-auto'>
+                <h3>Total Price:$ {cartTotal}</h3>
+        </div>
+        <div className='col-auto'>
+                <button className='btn btn-danger m-2'
+                        onClick={()=>{
+                            emptyCart(0)
+                        }}> Clear  Cart</button>
+               <button className='btn btn-primary m-2'>Buy Now</button>
         </div>
     </div>
     </section>
